daa4798216ce135a1ced238c18812dddbcbe7cbf
VinaiGreat <danteklifford@gmail.com>
2026-07-03 10:41:17 +0200

    test

---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
+
+// https://astro.build/config
+export default defineConfig({
+  adapter: vercel(),
+  integrations: [tailwind()],
+  server: {
+    port: 4321,
+    host: true
+  },
+  devToolbar: {
+    enabled: false
+  },
+  vite: {
+    server: {
+      headers: {
+        'Cache-Control': 'no-cache, no-store, must-revalidate'
+      }
+    }
+  }
+});
